f2406574fcc0c8e39f31fe48fdf075f2128e041a
Sebastiaan Luca <hello@sebastiaanluca.com>
2017-02-12 17:28:51 +0100

    Add StubGenerator
    Signed-off-by: Sebastiaan Luca <hello@sebastiaanluca.com>

---
+++ src/StubGenerator.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace SebastiaanLuca\FileGenerator;
+
+class StubGenerator
+{
+    /**
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * @var array
+     */
+    protected $replacements;
+
+    /**
+     * @param string $path
+     * @param array $replacements
+     */
+    public function __construct(string $path, array $replacements)
+    {
+        $this->path = $path;
+        $this->replacements = $replacements;
+    }
+
+    public function render()
+    {
+        $contents = file_get_contents($this->path);
+
+        collect($this->replacements)->each(function (string $replacement, string $tag) use (&$contents) {
+            $contents = str_replace($tag, $replacement, $contents);
+        });
+
+        ddd($contents);
+    }
+}
